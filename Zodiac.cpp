296f73730d412bf245a1e7decbd712621f756145
AJ <94884406+0rcino@users.noreply.github.com>
2023-08-29 16:41:07 +0800

    new fix code and comment

--- Zodiac.cpp
+++ Zodiac.cpp
@@ -1,22 +1,26 @@
-// Zodiac sign and Chinese zodiac sign with years and Birth Months and days
-// Created by Allen A.KA. AJ
+/*Create a Program that shall determine the Chinese Zodiac Sign (By year)
+and Zodiac Sign (By Month and Day) with Validation of Birthday, month and year.*/
+
+// Progam # 03
+// Created by Allen callsign AJ
+
 #include <iostream>
 using namespace std;
 
 // use string and fucntion
 string zodiacsSigns(int months, int days) // use int for whole number
 {
-  // Define the start and end dates of each zodiac sign
-  int zodiacDates[12] = {21, 20, 21, 21, 22, 22, 23, 24, 24, 24, 23, 22};
+
+  int zodiacDates /*use array*/[12] = {21, 20, 21, 21, 22, 22, 23, 24, 24, 24, 23, 22};
   string zodiacsSigns /*use omit array*/[12] = {"Aquarius", "Pisces", "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn"};
 
   // use if to Check the birth date is valid or true
   if (months < 1 || months > 12 || days < 1 || days > 31)
   {
-    return "Invalid date";
+    return "Invalid date";// validation 
   }
 
-  if (days < zodiacDates[months - 1]) // use if a condition is true
+  if (days < zodiacDates[months - 1]) 
   {
     return zodiacsSigns[(months + 10) % 12];
   }
@@ -27,14 +31,14 @@ string zodiacsSigns(int months, int days) // use int for whole number
 }
 
 // use string and function
-string chinesesZodiacSigns(int years)
+string chineseZodiac(int years) // use int for whole number
 {
   // Define the cycle of the Chinese zodiac signs
   string chinesesZodiac /*use omit array*/[12] = {"Monkey", "Rooster", "Dog", "Pig", "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Sheep"};
 
-  if (years < 12) // use if to declare a condition is true
+  if (years < 0) // use if to declare a condition is true
   {
-    return "Invalid year";
+    return "Invalid year"; // validation 
   }
   return chinesesZodiac[years % 12];
 }
@@ -52,5 +56,5 @@ int main() // main of the function of code
   cin >> years;
 
   cout << "Your zodiac sign is: " << zodiacsSigns /*call the function*/ (months, days) << endl;
-  cout << "Your Chinese zodiac sign is: " << chinesesZodiacSigns /*call the function*/ (years) << endl;
+  cout << "Your Chinese zodiac sign is: " << chineseZodiac /*call the function*/ (years) << endl;
 }

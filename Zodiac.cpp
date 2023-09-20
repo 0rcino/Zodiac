560eec73fffb540225f56203fed2c9240f58d97f
AJ <94884406+0rcino@users.noreply.github.com>
2023-09-20 17:47:07 +0800

    new change and add leap year

--- Zodiac.cpp
+++ Zodiac.cpp
@@ -4,53 +4,54 @@ and Zodiac Sign (By Month and Day) with Validation of Birthday, month and year.*
 // Progam # 03
 
 /*Leader: Allen Jefferson C. Orcino
+  Member:
+
  */
 
 #include <iostream>
 using namespace std;
 
-string zodiacsSigns(int months, int days) // use string and function use in for whole number
+string zodiacsSigns(int months, int days)
 {
-  // Define the cycle of the Zodiac signs
-  int zodiacDates /*use array*/[12] = {21, 20, 21, 21, 22, 22, 23, 24, 24, 24, 23, 22};
-  string zodiacsSigns /*use omit array*/[12] = {"Aquarius", "Pisces", "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn"};
 
-  // use conditional statement check the birth date is valid or true
+  int zodiacDates[12] = {21, 20, 21, 21, 22, 22, 23, 24, 24, 24, 23, 22};
+  string zodiacsSigns[12] = {"Aquarius", "Pisces", "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn"};
+
   if (months < 1 || months > 12 || days < 1 || days > 31)
   {
-    return "Invalid date"; // validation
+    return "Invalid date";
   }
 
   if (days < zodiacDates[months - 1])
   {
     return zodiacsSigns[(months + 10) % 12];
   }
-  else // use else a conditon is false
+  else
   {
     return zodiacsSigns[months - 1];
   }
 }
 
-string chineseZodiac(int years) // use string and function
+string chineseZodiac(int years)
 {
-  // Define the cycle of the Chinese Zodiac signs
-  string chinesesZodiac[12] /*use omit array*/ = {"Monkey", "Rooster", "Dog", "Pig", "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Sheep"};
 
-  if (years < 0) // use if to declare a condition is true
+  string chinesesZodiac[12] = {"Monkey", "Rooster", "Dog", "Pig", "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Sheep"};
+
+  if (years < 0)
   {
     return "Invalid year";
   }
   return chinesesZodiac[years % 12];
 }
 
-bool leapYear(int years) // validated for leap year or months or days about months
+bool leapYears(int years)
 {
-  return (years % 4 == 0 && years % 100 != 0) || (years % 400 == 0); // to declare to leap year
+  return (years % 4 == 0 && years % 100 != 0) || (years % 400 == 0);
 }
 
-bool valid(int days, int months, int years)
+bool inValids(int days, int months, int years)
 {
-  if (years < 1900 || years > 2100 || months < 1 || months > 12 || days < 1 || days > 31) // input of year to declare leap year and months
+  if (years < 1900 || years > 2100 || months < 1 || months > 12 || days < 1 || days > 31)
     return false;
 
   int daysAndMonth = 31;
@@ -58,7 +59,7 @@ bool valid(int days, int months, int years)
     daysAndMonth = 30;
   else if (months == 2)
   {
-    if (leapYear(years))
+    if (leapYears(years))
       daysAndMonth = 29;
     else
       daysAndMonth = 28;
@@ -67,13 +68,11 @@ bool valid(int days, int months, int years)
   return days <= daysAndMonth;
 }
 
-int main() // main of the function of code
+int main()
 {
 
-  int months, days, years; // int for whole number
+  int months, days, years;
 
-  /*  do/while loop is a variant of the while loop. This loop will execute the code block once,
-  before checking if the condition is true, then it will repeat the loop as long as the condition is true.*/
   do
   {
     cout << "Zodiac and Chinese Sign";
@@ -86,7 +85,7 @@ int main() // main of the function of code
     cout << "Enter your birth year: (1900-2100): ";
     cin >> years;
 
-    if (!valid(days, months, years)) // if wrong input declare to invalid date
+    if (!inValids(days, months, years))
     {
       cout << "Please enter a valid birthday date." << endl;
       cin.get();
@@ -95,10 +94,10 @@ int main() // main of the function of code
       cout << endl;
       cin.get();
     }
-  } while (!valid(days, months, years)); // condtion for days, months, years
+  } while (!inValids(days, months, years));
 
-  cout << "Your zodiac sign is: " /*call a function*/ << zodiacsSigns(months, days) << endl;
-  cout << "Your Chinese zodiac sign is: " /*call a function*/ << chineseZodiac(years) << endl;
+  cout << "Your zodiac sign is: " << zodiacsSigns(months, days) << endl;
+  cout << "Your Chinese zodiac sign is: " << chineseZodiac(years) << endl;
   cin.get();
   cout << endl;
   cout << "To return to Choices, press ENTER . . . ";
